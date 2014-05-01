6b5af65a3d238cffc164123b3eec9716deb77c0f
Joris Philipsen <info@jorisphilipsen.nl>
2014-05-01 15:18:57 +0200

    v0.2 - I need to add some demo's and documentation also fix bugs

--- JQueryEqualHeight.js
+++ JQueryEqualHeight.js
@@ -1,28 +1,90 @@
 (function ( $ ) {
 
 	$.fn.EqualHeight = function( options ) {
-		
-		//Verzamel alle hoogtes en maak ze allemaal even groot als de hoogste.
+
+		// By default the plugin is responsive.
+		var settings = $.extend({
+			responsive: true
+		}, options );
+	
+		//Assign this to element to keep plugin working, need to fix this.
+		var element = this;
+
+		//Collect all heights of the elements and give them the same height as the tallest element
 		var arrayTextHeight = [];
 		var largestTextHeight;
 
-		this.each(function() {
-			//De removeAttr is om de vorige hoogte te resetten.
-			$(this).removeAttr('style');
-			arrayTextHeight.push($(this).height());
-		});
+		function createEqualHeight(){
 
-		//Pak de hoogste uit de array.
-		largestTextHeight = Math.max.apply(Math, arrayTextHeight);
+			element.each(function() {
+				//The removeAttr is needed to reset the height of the elements
+				$(this).removeAttr('style');
+				arrayTextHeight.push($(this).height());
+			});
 
-		this.height(largestTextHeight);
-		
-		//Maak de array weer leeg zodat deze kan worden gevuld met nieuwe hoogtes
-		arrayTextHeight.length = 0;
+			//Get the highest value from the array
+			largestTextHeight = Math.max.apply(Math, arrayTextHeight);
+
+			element.height(largestTextHeight);
+
+			//Clear the array so it can be filled with new values.
+			arrayTextHeight.length = 0;
+
+		}
+
+		if (settings.responsive) {
+
+			//Resize pas na een half seconde, maakt de website sneller. Na 500 milliseconden voert hij de doneResizing functie uit.
+			var timer;
+			$(window).resize(function() {
+				clearTimeout(timer);
+				timer = setTimeout(createEqualHeight, 500);
+			});
+		}
+
+		//Equals height of elements on the page load.
+		createEqualHeight();
 
 	};
 
 }( jQuery ));
+
+
+// function createEqualHeight(){
+// 	this.each(function() {
+// 		//De removeAttr is om de vorige hoogte te resetten.
+// 		$(this).removeAttr('style');
+// 		arrayTextHeight.push($(this).height());
+// 	});
+
+// 	//Pak de hoogste uit de array.
+// 	largestTextHeight = Math.max.apply(Math, arrayTextHeight);
+
+// 	this.height(largestTextHeight);
+	
+// 	//Maak de array weer leeg zodat deze kan worden gevuld met nieuwe hoogtes
+// 	arrayTextHeight.length = 0;
+// }
+
+
+// this.each(function() {
+// 	//De removeAttr is om de vorige hoogte te resetten.
+// 	$(this).removeAttr('style');
+// 	arrayTextHeight.push($(this).height());
+// });
+
+// //Pak de hoogste uit de array.
+// largestTextHeight = Math.max.apply(Math, arrayTextHeight);
+
+// this.height(largestTextHeight);
+
+// //Maak de array weer leeg zodat deze kan worden gevuld met nieuwe hoogtes
+// arrayTextHeight.length = 0;
+
+
+
+
+
 
 
 // (function ($, Drupal, window, document, undefined) {
